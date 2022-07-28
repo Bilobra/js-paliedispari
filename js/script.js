b0dd1fc6a974a7eea5ac8ab339d76f2105e36b2d
Bilobra <bra.carrozzo@gmail.com>
2022-07-28 14:41:13 +0200

    add palidroma function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 // console di prova
 console.log('provaaaaa mi sentiii?')
+
+// PALIDROMA
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let parola = prompt('inserisci una parola ');
+
+console.log(parola)
+
+// funzione dove la parola è splittata , invertita e composta in un unica operazione
+
+function invertiParola (stringa){
+    const stringaInversa = stringa.split('').reverse().join('')
+   
+    return stringaInversa
+}
+// salviamo i dati in una costante 
+const parolaInversa = invertiParola(parola)
+console.log(parolaInversa)
+
+// verifichiamo se la parola è palidroma con una condizione
+if (parola === parolaInversa){
+    console.log('la parola è palindroma')
+} else {
+    console.log('la parola non è palindroma')
+}
+
+// ---------------------------
+
+// PARI & DISPARI
